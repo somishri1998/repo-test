aca6e8312ba2edfef1e48314c2b11df1c95b6433
Somi Srivastva <somi.Srivastva@civicplus.com>
2021-11-19 21:29:13 +0530

    first commit

---
+++ Github_Xcode_Demo/GithubXcodeDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Github Xcode Demo
+//
+//  Created by Paul Solt on 3/2/16.
+//  Copyright © 2016 Super Easy Apps LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello!")
+        
+        // Added 1st statement on Branch Update-the-UI
+    
+        doSomething()
+    }
+    
+    func doSomething() {
+        print("do some algorithm and computation")
+    }
+
+}
+
